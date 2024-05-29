93ffbcc1f3778f02633d5bb4963b6377b44d1e91
leorasaharia <saharialeora2406@gmail.com>
2024-05-29 16:55:10 +0530

    index.js was updated

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -5,24 +5,52 @@ import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
 import { Dimensions, Image, Linking, Modal, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
+// Import your images manually
+import image1 from '@/assets/images/1.jpg';
+import image10 from '@/assets/images/10.jpg';
+import image11 from '@/assets/images/11.jpg';
+import image12 from '@/assets/images/12.jpg';
+import image13 from '@/assets/images/13.jpg';
+import image14 from '@/assets/images/14.jpg';
+import image15 from '@/assets/images/15.jpg';
+import image16 from '@/assets/images/16.jpg';
+import image2 from '@/assets/images/2.jpg';
+import image3 from '@/assets/images/3.jpg';
+import image4 from '@/assets/images/4.jpg';
+import image5 from '@/assets/images/5.jpg';
+import image6 from '@/assets/images/6.jpg';
+import image7 from '@/assets/images/7.jpg';
+import image8 from '@/assets/images/8.jpg';
+import image9 from '@/assets/images/9.jpg';
+
 export default function HomeScreen() {
   const navigation = useNavigation();
   const [modalVisible, setModalVisible] = useState(false);
 
   const screenWidth = Dimensions.get('window').width;
-  const imageSize = (screenWidth - 10) / 8; // Making the images very small
+  const imageSize = (screenWidth - 10) / 10; // Making the images very small
 
-  const images = [];
+  // Create an array of image objects
+  const images = [
+    { id: '1', src: image1 },
+    { id: '2', src: image2 },
+    { id: '3', src: image3 },
+    { id: '4', src: image4 },
+    { id: '5', src: image5 },
+    { id: '6', src: image6 },
+    { id: '7', src: image7 },
+    { id: '8', src: image8 },
+    { id: '9', src: image9 },
+    { id: '10', src: image10 },
+    { id: '11', src: image11 },
+    { id: '12', src: image12 },
+    { id: '13', src: image13 },
+    { id: '14', src: image14 },
+    { id: '15', src: image15 },
+    { id: '16', src: image16 },
+  ];
 
-  // Loop to generate image objects for remaining images
-  for (let i = 1; i <= 15; i++) {
-      const id = String(i);
-      const src = require(`@/assets/images/${i}.jpg`);
-      images.push({ id, src });
-  }
-  
   console.log(images);
-  
 
   const handleTrashpediaPress = () => {
     Linking.openURL('https://sites.google.com/view/trashtrek/home');
